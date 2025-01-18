85d881198ed433a42b7cdd3499614383b2b3b8d8
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2025-01-18 20:19:51 +0900

    no message

---
+++ ManagedDxlGame/program/library/sgl/sgl_actor.cpp
@@ -0,0 +1,95 @@
+﻿#include "DxLib.h"
+#include "sgl_actor.h"
+#include "sgl_component.h"
+#include "sgl_level.h"
+
+Actor::Actor() {}
+Actor::~Actor() {}
+
+#pragma region virtual-funcitons
+void Actor::Initialize() {}
+void Actor::Update(float deltaTime) {}
+void Actor::Release() {}
+void Actor::Finalize() {}
+void Actor::Draw() {}
+#pragma endregion
+
+void Actor::__initialize()
+{
+	auto it = m_components.begin();
+	while (it != m_components.end())
+	{
+		(*it)->Initialize();
+		it++;
+	}
+	m_enabled = true;
+	Initialize();
+}
+
+void Actor::__update(float deltaTime)
+{
+	auto it = m_components.begin();
+	while (it != m_components.end())
+	{
+		(*it)->Update(deltaTime);
+		it++;
+	}
+	Update(deltaTime);
+}
+
+void Actor::__draw()
+{
+	auto it = m_components.begin();
+	while (it != m_components.end())
+	{
+		(*it)->Draw();
+		it++;
+	}
+	Draw();
+}
+
+void Actor::__release()
+{
+	auto it = m_components.begin();
+	while (it != m_components.end())
+	{
+		(*it)->Release();
+		it++;
+	}
+	m_components.clear();
+	Release();
+}
+
+void Actor::__finalize()
+{
+	auto it = m_components.begin();
+	while (it != m_components.end())
+	{
+		(*it)->Finalize();
+		it++;
+	}
+	m_enabled = false;
+	m_placedLevel = nullptr;
+	Finalize();
+}
+
+
+std::list< Component* >::iterator
+const Actor::AddComponent(Component* component)
+{
+	component->SetActor(this);
+	m_components.emplace_back(component);
+	auto it = m_components.end();
+	it--;
+	return it;
+}
+
+void const Actor::RemoveComponent(const std::list<Component*>::iterator place)
+{
+	m_components.erase(place);
+}
+
+void const Actor::SetPlacedLevel(const Level* level)
+{
+	m_placedLevel = const_cast<Level*>(level);
+}
---
+++ ManagedDxlGame/program/library/sgl/sgl_level.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "list"
+#include "memory"
+#include "sgl_fpsCamera.h"
+#include "sgl_lang_extention.h"
+#include "sgl_managableModules.h"
+// 前方定義
+class Actor;
+// Unityでいうシーン、すべてのアクタが存在するクラス
+class Level final : IManagableModule {
+private:
+	std::list< Actor* > m_actors;
+	std::shared_ptr<FPSCamera> m_camera;
+public:
+	Level();
+	~Level();
+
+	std::shared_ptr<FPSCamera> mainCamera() { return m_camera; }
+
+	static Level* Create() {
+		return new Level;
+	}
+
+	DEF_Create_shared_ptr(Level)
+public:
+	void  Initialize();
+	void  Update(float deltaTime);
+	void  Draw();
+	void  Finalize();
+	void  Release();
+
+	void DrawActorList();
+	const std::list< Actor* >::iterator
+		const AddActor(const Actor* obj);
+	void const RemoveActor(const Actor* obj);
+	void const RemoveActor(const std::list< Actor* >::iterator place);
+};
